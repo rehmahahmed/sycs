b6ab13f41befdc93578846d134cb987030304457
Rehmah <95929046+rehmahahmed@users.noreply.github.com>
2023-03-29 17:30:13 +0530

    Delete prac3B.js

--- prac3B.js
+++
@@ -1,9 +0,0 @@
-var ar = require("./arithematic");
-var http = require("http");
-http.createServer(function(req, res){
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.write("Addition: 10 + 5: ", ar.add(10, 5));
-    res.write("Subtraction: 10 - 5: ", ar.subtract(10, 5));
-    res.write("Multiplication: 10 * 5: ", ar.multiply(10, 5));
-    res.write("Division: 10 / 5: ", ar.divide(10, 5));
-}).listen(8888);
